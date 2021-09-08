78ab77f586a4f029b1bbb95a2198736f322718b7
Milena <milena20182018@gmail.com>
2021-09-08 06:02:04 -0300

    Instalando as dependências

--- apires/lib/main.dart
+++ apires/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+
+import 'model/users.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,54 +16,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Dio dio = Dio();
+  // User userData;
+  @override
+  void initState() { // Sera executado antes de gerar o context
+    // TODO: implement initState
+    getData();
+    super.initState();
   }
 
+  Future<void> getData() async {
+    Response response = await dio.get("https://jsonplaceholder.typicode.com/todos");
+    print(response);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Text(index.toString()),
+          );
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
---
+++ apires/lib/model/users.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'users.g.dart';
+
+@JsonSerializable()
+class User{
+  int userId;
+  int id;
+  String title;
+  bool completed;
+
+  User({required this.userId, required this.id, required this.title, required this.completed});
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
